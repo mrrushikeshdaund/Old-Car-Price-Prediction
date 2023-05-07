86d2525b2d884d8cfe2ac915c5b778ac3cf83470
Rushikesh Daund <rushikesh.daund@yahoo.com>
2023-05-07 12:31:33 +0530

    remove pycache folder

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -47,10 +47,10 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 
-if __name__=='__main__':
+"""if __name__=='__main__':
     obj = DataIngestion()
     train_data_path , test_data_path = obj.initiate_data_ingestion()
     print(" Data Ingestion Done Sucessfully ")
-
+"""
 
 
